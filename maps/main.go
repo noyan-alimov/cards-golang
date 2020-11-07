a323109488fe39fedc1f1c538dfb8bf85e05285a
Noyan Alimov <noyan.alimov@gmail.com>
2020-11-07 19:32:10 +0000

    covered maps section

--- maps/main.go
+++ maps/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "net/http"
+import "fmt"
 
 func main() {
-	http.ListenAndServe("8000", nil)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, hex)
+	}
 }
