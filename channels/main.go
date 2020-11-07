be46d372b9e303e8236df3ac020e4d1965389af1
Noyan Alimov <noyan.alimov@gmail.com>
2020-11-07 22:24:28 +0000

    learned about go routines and channels

---
+++ channels/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://facebook.com",
+		"http://amazon.com",
+		"http://golang.org",
+		"http://stackoverflow.com",
+		"http://google.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "is down")
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up and working")
+	c <- link
+}
